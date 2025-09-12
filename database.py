3ab1df292dc1512ab9fd7c8b88a0005d2d312da7
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-12 12:40:22 +0100

    backend code commit by ENGR. IPAYE

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
 from sqlmodel import SQLModel, create_engine, Session
+
+# SQLite file
+DATABASE_URL = "sqlite:///./notes.db"
+
+connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, echo=False, connect_args=connect_args)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    return Session(engine)
