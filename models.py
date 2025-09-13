ddf45402e159b2f37f6d1dc4993511f9414b659a
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-13 09:52:09 +0100

    backend code commit by ENGR. IPAYE

--- models.py
+++ models.py
@@ -1,6 +1,15 @@
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class User(SQLModel, table=False):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    password_hash: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    notes: List["Note"] = Relationship(back_populates="owner")
 
 
 class Note(SQLModel, table=True):
@@ -12,3 +21,6 @@ class Note(SQLModel, table=True):
     content_type: Optional[str] = None
     size: Optional[int] = None
     created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    owner_id: int = Field(foreign_key="user.id")
+    owner: Optional[User] = Relationship(back_populates="notes")
